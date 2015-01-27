3f64581766357fa47399758680acdde33aa2c0da
Dan Fitzgerald <danfitz7@hotmail.com>
2015-01-27 00:44:43 -0500

    Input working with files piping

--- shell.c
+++ shell.c
@@ -19,7 +19,7 @@ unsigned long timeval2long(struct timeval* timeVal){
 	return timeVal->tv_sec*1000 + timeVal->tv_usec/1000;
 }
 
-// execute the given command with the given arguments
+// Execute the given command with the given arguments and print its statistics.
 void executeShellCommand(const char* strCommand, const char*   arguments[]){
 	//store child process statistics
 	struct timeval start;
@@ -50,12 +50,38 @@ void executeShellCommand(const char* strCommand, const char*   arguments[]){
 	}
 }
 
+// Change the working directory of the shell to the given path.
+void changeDirectory(char* strPath){
+	if (strPath!=NULL){
+		printf("Changing working directory to '%s'\n", strPath);
+		if (chdir(strPath) !=0){
+			printf("ERROR: bad path: '%s'.", strPath);
+		}
+	}else{
+		printf("ERROR: no new directory given.\n");
+	}
+}
+
+int exitShell(){
+	printf("\nExiting shell...\n");
+	exit(0);
+	return 0;
+}
+
+#define MAX_LINE_LENGTH 128
+char buffer[MAX_LINE_LENGTH+2]; 	// Buffer to hold input line characters, plus an extra, plus a NULL termination
+char* promptForInput(){
+	printf("Enter a command and its arguments:");
+
+	// read the next line from standard input, store in the input buffer.
+	return fgets(buffer, sizeof(buffer)+1, stdin);
+}
+
 #define MAX_LINE_LENGTH 128
 #define MAX_ARGS 32
 int main(int argc, const char* argv[]){
 
 	#define delimiters " \n\0"	// Separate arguments with these delimiters
-	char buffer[MAX_LINE_LENGTH+2]; 	// Buffer to hold input line characters, plus an extra, plus a NULL termination
 	char* strCommand;			// The command string is extracted from stdin every loop
 	char* strArgument;			// Each argument to the command is extracted here before being copied to the arguments array
 	char* arguments[MAX_ARGS+1]; // Buffer to hold arguments as they are being parsed
@@ -63,16 +89,12 @@ int main(int argc, const char* argv[]){
 	printf("Starting shell...\n");
 
 	//Read, Evaluate, Print, Loop.
-	while(1){
-		printf("Enter a command and its arguments:");
+	while((promptForInput())){
 
-		// read the next line from standard input, store in the input buffer.
-		fgets(buffer, sizeof(buffer)+1, stdin);
 		if (strlen(buffer)>MAX_LINE_LENGTH+1){
 			printf("ERROR: maximum input length exceeded (%d chars).\n", strlen(buffer)-1);
 
 			//clear the rest of the input buffer
-			//fflush(stdin);
 			int c;
 			while ((c = fgetc(stdin)) != EOF && c != '\n'); //http://stackoverflow.com/questions/7898215/how-to-clear-input-buffer-in-c
 		}else{
@@ -81,47 +103,60 @@ int main(int argc, const char* argv[]){
 			// extract the command
 			char* chompedBuffer = strtok(buffer,"\n");		//chop off trailing newline
 			strCommand = strtok(chompedBuffer, delimiters);	//extract the command as the first token (whitespace-seperated word)
-			printf("Command is '%s' (%d characters)\n", strCommand, MAX_LINE_LENGTH);
+//			printf("Command is '%s' (%d characters)\n", strCommand, MAX_LINE_LENGTH);
 
-			// loop through input line, extracting and counting arguments
-			int max_arg_i = 0;	//number of arguments parsed so far
-			strArgument = strtok(NULL, delimiters); 	//attempt to parse the next token (first argument)
-			int MAX_ARGS_Error = 0;
-			while (strArgument!=NULL){
-	//			printf("Next token...\n");
-
-				if (max_arg_i+1 > MAX_ARGS){
-					printf("ERROR: Maximum number of arguments exceeded.\n");
-					MAX_ARGS_Error = 1;
-					break;
-				}
-
-				//make a new string to hold the argument, copy from arguments buffer
-				size_t argument_length = strlen(strArgument); //chompedBuffer-strArgument
-				printf("\tArgument %d:'%s' has %d characters\n", max_arg_i, strArgument, argument_length);
-				arguments[max_arg_i] = (char*) malloc(argument_length+1);
-				strcpy(arguments[max_arg_i], strArgument);
-				strArgument = strtok(NULL, delimiters);
-				max_arg_i++;
+			// detect shell exit
+			if (strcmp(strCommand, "exit") == 0){
+				exitShell();
 			}
-			if (!MAX_ARGS_Error){
-				printf("Received %d arguments for command %s\n", max_arg_i, strCommand);
 
-				//debug print
-				printf("Arguments buffer is...\n");
-				for (int i=0;i<max_arg_i;i++){
-					printf("%s\n", arguments[i]);
+			// get the first argument (if there is one)
+			strArgument = strtok(NULL, delimiters); 	//attempt to parse the next token (first argument)
+
+			//catch shell change directory, give it the path
+			if (strcmp(strCommand, "cd") == 0){
+				changeDirectory(strArgument);
+			}else{
+
+				// loop through input line, extracting and counting arguments
+				int max_arg_i = 0;	//number of arguments parsed so far
+				int MAX_ARGS_Error = 0;
+				while (strArgument!=NULL){
+		//			printf("Next token...\n");
+
+					if (max_arg_i+1 > MAX_ARGS){
+						printf("ERROR: Maximum number of arguments exceeded.\n");
+						MAX_ARGS_Error = 1;
+						break;
+					}
+
+					//make a new string to hold the argument, copy from arguments buffer
+					size_t argument_length = strlen(strArgument); //chompedBuffer-strArgument
+//					printf("\tArgument %d:'%s' has %d characters\n", max_arg_i, strArgument, argument_length);
+					arguments[max_arg_i] = (char*) malloc(argument_length+1);
+					strcpy(arguments[max_arg_i], strArgument);
+					strArgument = strtok(NULL, delimiters);
+					max_arg_i++;
 				}
+				if (!MAX_ARGS_Error){
+//					printf("Received %d arguments for command %s\n", max_arg_i, strCommand);
 
-				arguments[max_arg_i+1] = NULL; //last arg is null pointer
-				//executeShellCommand(strCommand, arguments);
+					//debug print
+					/*printf("Arguments buffer is...\n");
+					for (int i=0;i<max_arg_i;i++){
+						printf("\t%s\n", arguments[i]);
+					}*/
 
-				//free memory from the arguments
-				for (int i=0;i<max_arg_i;i++){
-					free(arguments[i]);
+					arguments[max_arg_i+1] = NULL; //last arg is null pointer
+					//executeShellCommand(strCommand, arguments);
+
+					//free memory from the arguments
+					for (int i=0;i<max_arg_i;i++){
+						free(arguments[i]);
+					}
 				}
 			}
 		}
 	}
-	return 0;
+	exitShell();
 }
